c762b83c742eb474f1a6fe3a8559ffaa948d1c93
Your Name <zaagagon@gmail.com>
2023-02-24 07:51:59 -0500

    probando branch pruebas funcion flecha suma

---
+++ flecha1.js
@@ -0,0 +1,12 @@
+//cambios en git emailst
+function mul(x) { return x * 2; }
+salida = mul(4)
+console.log(salida)
+
+//y => y * 2
+let producto =y=>y*2
+//let sum = (a, b) => a + b;
+console.log("resultado es : "+producto(5))
+
+let suma=(a,b)=>a+b;
+console.log(suma(2,3))
