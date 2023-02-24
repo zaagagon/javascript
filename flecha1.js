a1236ad6ffac0613b9aeb6c05c75543bb1938b19
Your Name <zaagagon@gmail.com>
2023-02-24 07:41:52 -0500

    funcion flecha producto

--- flecha1.js
+++ flecha1.js
@@ -3,5 +3,7 @@ function mul(x) { return x * 2; }
 salida = mul(4)
 console.log(salida)
 
-y => y * 2
+//y => y * 2
+let producto =y=>y*2
 //let sum = (a, b) => a + b;
+console.log("resultado es : "+producto(5))
