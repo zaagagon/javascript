97d878421dc2f7419bd1ed4e26ace991e0a45d38
Your Name <zaagagon@gmail.com>
2023-03-01 18:15:20 -0500

    programming

---
+++ solucion.js
@@ -0,0 +1,20 @@
+/*Algoritmo detarea
+	Definir x,mayores,menores Como Entero
+	mayores = 0
+	menores = 0
+	para x = 1 Hasta 200 Con Paso 1 Hacer
+		Escribir "Ingresa una edad"
+		leer edad
+		si edad > 18 Entonces
+			mayores = mayores + 1
+		SiNo
+			menores = menores + 1
+		FinSi
+	FinPara
+	Escribir "Personas mayores de edad: ",mayores
+	Escribir "Personas menores de edad: ",menores
+FinAlgoritmo*/
+
+var nombre ="piton"
+prompt("quien eres")
+console.log(nombre)
