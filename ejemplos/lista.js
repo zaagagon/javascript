7c934d0e1f220cd3c79b8ccc2e27c6f747b392fe
zaagagon <zaagagon@gmail.com>
2025-09-30 12:33:32 -0500

    cambios en lista

--- ejemplos/lista.js
+++ ejemplos/lista.js
@@ -1,7 +1,17 @@
 //serie=[1,2,3,4,5]
 //estructura bucle con js
-for (let i = 0; i <6; i++) {
+/*for (let i = 0; i <6; i++) {
     console.log("Iteración :" +i );
     
     
+}*/
+
+
+//programa que imprime los numeros del 1 al 10
+
+for(let i=0 ;i<11;i++){
+    //instrucción
+    console.log(i);
+    
+
 }
