bdcac69d09b396debfc5b2b66e35de61c1f52683
zaagagon <zaagagon@gmail.com>
2025-10-08 22:45:23 -0500

    js con quokka

---
+++ flecha2.js
@@ -0,0 +1,2 @@
+const suma=(a,b)=>a+b;
+console.log("Resultado de la suma :" + suma(5,3));
---
+++ flecha3.js
@@ -0,0 +1,14 @@
+const producto=(a,b)=>a*b;
+producto();
+console.log(producto(5,6));
+x=10
+y=9
+x + y
+
+function mensaje() {
+    console.log("prueaba");
+    
+    
+}
+mensaje();
+y + x
